4d6bb9f81583a4dc974d952d5ce62ec1ba62a13b
ardiman-xyz <pc.ardiman98@gmail.com>
2024-07-09 20:59:26 +0800

    ft: add setting account adn avatar form

--- resources/js/Pages/Settings/Index.tsx
+++ resources/js/Pages/Settings/Index.tsx
@@ -1,18 +1,18 @@
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import {GeneralInfo} from "@/Pages/Settings/_components/GeneralInfo";
-import {Head} from "@inertiajs/react";
-import {Separator} from "@/Components/ui/separator";
-import {AccountSetting} from "@/Pages/Settings/_components/Account";
+import { GeneralInfo } from "@/Pages/Settings/_components/GeneralInfo";
+import { Head } from "@inertiajs/react";
+import { Separator } from "@/Components/ui/separator";
+import { AccountSetting } from "@/Pages/Settings/_components/Account";
 
 const SettingPage = () => {
     return (
         <Authenticated>
-            <Head title="Pengaturan"/>
+            <Head title="Pengaturan" />
             <h1 className="text-3xl font-bold">Pengaturan</h1>
 
             <div className=" ">
                 <div className="flex flex-col gap-y-4 mt-10">
-                    <GeneralInfo/>
+                    <GeneralInfo />
                 </div>
                 <Separator />
                 <div className="mt-10 mb-20">
@@ -20,7 +20,7 @@ const SettingPage = () => {
                 </div>
             </div>
         </Authenticated>
-    )
-}
+    );
+};
 
 export default SettingPage;
--- resources/js/Pages/Settings/_components/Account.tsx
+++ resources/js/Pages/Settings/_components/Account.tsx
@@ -1,12 +1,11 @@
-import {AccountForm} from "@/Pages/Settings/_components/AccountForm";
-import {Separator} from "@/Components/ui/separator";
-import {Personalnfo} from "@/Pages/Settings/_components/Personalnfo";
-import {AvatarForm} from "@/Pages/Settings/_components/AvatarForm";
+import { AccountForm } from "@/Pages/Settings/_components/AccountForm";
+import { Separator } from "@/Components/ui/separator";
+import { Personalnfo } from "@/Pages/Settings/_components/Personalnfo";
+import { AvatarForm } from "@/Pages/Settings/_components/AvatarForm";
 
 export const AccountSetting = () => {
     return (
         <>
-
             <div className="grid lg:grid-cols-2 grid-cols-1 gap-x-10 lg:gap-y-0 gap-y-10">
                 <div>
                     <Personalnfo />
@@ -18,5 +17,5 @@ export const AccountSetting = () => {
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
