ec773fae082a0915a8b65e202126434a01e3595c
ardiman-xyz <pc.ardiman98@gmail.com>
2024-07-10 14:06:17 +0800

    remove react-icons dependencies

--- resources/js/Pages/Auth/Social/_components/google-action.tsx
+++ resources/js/Pages/Auth/Social/_components/google-action.tsx
@@ -1,19 +1,16 @@
-
-import {FcGoogle} from "react-icons/fc";
-
+// import { FcGoogle } from "react-icons/fc";
 
 const GoogleActionLogin = () => {
-
     return (
         <div>
-            <a href={route("social.auth.redirect")}>
+            {/* <a href={route("social.auth.redirect")}>
                 <div className="w-[313px] bg-gray-100 border border-gray-300 px-4 py-3 rounded cursor-pointer flex items-center justify-center hover:bg-gray-200">
                     <FcGoogle className="w-5 h-5 fill-gray-500 stroke-white" />
                     <p className="text-sm font-semibold ml-2 text-gray-700">Masuk dengan google</p>
                 </div>
-            </a>
+            </a> */}
         </div>
-    )
-}
+    );
+};
 
-export default GoogleActionLogin
+export default GoogleActionLogin;
