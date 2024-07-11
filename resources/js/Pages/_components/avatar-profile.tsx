89d2a748edb7d414d1b61c3b6cd6a65c01a7d9ad
ardiman-xyz <pc.ardiman98@gmail.com>
2024-07-12 06:07:27 +0800

    ft: dynamic profile info dashboard

--- resources/js/Pages/_components/avatar-profile.tsx
+++ resources/js/Pages/_components/avatar-profile.tsx
@@ -1,18 +1,26 @@
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar"
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar";
+import { SharedInertiaData } from "@/types/inertia";
+import { usePage } from "@inertiajs/react";
 
 export const AvatarProfile = () => {
+    const { auth } = usePage<SharedInertiaData>().props;
+
+    const school = auth?.school;
+
+    if (!school) return;
+
     return (
         <div className="w-full bg-[#0369A1] py-6 px-6 text-white rounded-md flex items-center gap-x-2">
             <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarImage src={"/storage/" + auth?.user?.profile_picture} />
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             <div className="flex flex-col gap-y-1">
-                <h3 className="text-md font-semibold truncate w-[250px]">Dikman Maheng, S.T., M.Eng</h3>
+                <h3 className="text-md font-semibold truncate w-[250px]">
+                    {school.leader_name}
+                </h3>
                 <p className="text-sm">Kepala Sekolah</p>
             </div>
         </div>
-    )
-}
+    );
+};
--- resources/js/Pages/_components/school-profile.tsx
+++ resources/js/Pages/_components/school-profile.tsx
@@ -1,17 +1,22 @@
+import { SharedInertiaData } from "@/types/inertia";
+import { usePage } from "@inertiajs/react";
 
 export const SchoolProfile = () => {
+    const { auth } = usePage<SharedInertiaData>().props;
+
+    const school = auth?.school;
+
+    if (!school) return;
+
     return (
-        <div
-            className="rounded bg-gradient-to-r from-sky-100 to-sky-50 py-6 px-8 border border-sky-100 mb-10 flex justify-between relative overflow-hidden">
+        <div className="rounded bg-gradient-to-r from-sky-100 to-sky-50 py-6 px-8 border border-sky-100 mb-10 flex justify-between relative overflow-hidden">
             <div className="md:w-1/2 w-full">
                 <h1 className="text-xl font-bold text-sky-800">
-                    SMAN 15 KONSEL
+                    {school.name}
                 </h1>
                 <p className="text-sm font-albert text-gray-700">
                     Selamat datang admin, di aplikasi{" "}
-                    <span className="font-semibold">
-                                            sispeng-tk.id
-                                        </span>
+                    <span className="font-semibold">sispeng-tk.id</span>
                 </p>
             </div>
             <div className="absolute md:flex hidden -top-3 right-10">
@@ -22,5 +27,5 @@ export const SchoolProfile = () => {
                 />
             </div>
         </div>
-    )
-}
+    );
+};
